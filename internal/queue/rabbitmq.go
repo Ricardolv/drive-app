b0464ad72a76937625bb9a36170bd0fea6792a1e
Ricardo Viana <richard@Ricardos-MacBook-Pro.local>
2024-05-22 18:06:09 -0300

    Implementando métodos para RabbitMQ

--- internal/queue/rabbitmq.go
+++ internal/queue/rabbitmq.go
@@ -1,27 +1,69 @@
 package queue
 
 import (
+	"context"
 	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-
 type RabbitMQConfig struct {
-	URL string
+	URL       string
 	TopicName string
-	Timeout time.Time
+	Timeout   time.Time
 }
 
 type RabbitmqConnection struct {
-	config RabbitMQConfig
+	config     RabbitMQConfig
 	connection *amqp.Connection
 }
 
 func (rc *RabbitmqConnection) Publish(message []byte) error {
 
+	channel, err := rc.connection.Channel()
+	if err != nil {
+		return err
+	}
+	defer channel.Close()
+
+	mp := amqp.Publishing{
+		DeliveryMode: amqp.Persistent,
+		Timestamp:    time.Now(),
+		ContentType:  "text/plain",
+		Body:         message,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	return channel.PublishWithContext(ctx,
+		"",
+		rc.config.TopicName,
+		false,
+		false,
+		mp)
 }
 
-func (rc *RabbitmqConnection) Conume() error {
+func (rc *RabbitmqConnection) Consume() error {
+
+	channel, err := rc.connection.Channel()
+	if err != nil {
+		return err
+	}
+	defer channel.Close()
+
+	queue, err := channel.QueueDeclare(rc.config.TopicName, false, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	msgs, err := channel.Consume(queue.Name, "", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	for d := range msgs {
+
+	}
 
 }
