04bfa2e89e2cdb0c0ab2f8fdb587f9d233bd7b9b
Ricardo Viana <richard@Ricardos-MacBook-Pro.local>
2024-05-27 12:36:28 -0300

    Definindo Handler

---
+++ internal/folders/routes.go
@@ -0,0 +1,16 @@
+package folders
+
+import (
+	"database/sql"
+
+	"github.com/go-chi/chi"
+)
+
+type handler struct {
+	db *sql.DB
+}
+
+func SetRoutes(r chi.Router, db *sql.DB) {
+	h := handler{db}
+
+}
