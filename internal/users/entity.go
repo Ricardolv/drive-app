50d61445dfb1b7d9773b8b4f6baac634086728f6
Ricardo Viana <richard@Ricardos-MacBook-Pro.local>
2024-05-27 09:08:15 -0300

    Implementando método SetPassword

--- internal/users/entity.go
+++ internal/users/entity.go
@@ -1,11 +1,21 @@
 package users
 
-import "time"
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"time"
+)
 
 func New(name, login, password string) (*User, error) {
 	now := time.Now()
 
 	user := User{Name: name, Login: login, CreatedAt: now, ModifiedAt: now}
+
+	err := user.SetPassword(password)
+	if err != nil {
+		return nil, err
+	}
 
 	return &user, nil
 }
@@ -19,4 +29,19 @@ type User struct {
 	ModifiedAt time.Time
 	Deleted    bool
 	LastLogin  time.Time
+}
+
+func (u *User) SetPassword(password string) error {
+
+	if password == "" {
+		return errors.New("Passwords is required and can't be blank")
+	}
+
+	if len(password) > 6 {
+		return errors.New("Password must have at least 6 characters")
+	}
+
+	u.Password = fmt.Sprintf("%x", (md5.Sum([]byte(password))))
+
+	return nil
 }
