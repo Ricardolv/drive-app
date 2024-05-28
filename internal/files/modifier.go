44f5d20c25825c100e74e52d0684e1465a92b576
Ricardo Viana <richard@Ricardos-MacBook-Pro.local>
2024-05-28 08:28:14 -0300

    Atualizando nome do arquivo

--- internal/files/modifier.go
+++ internal/files/modifier.go
@@ -2,8 +2,49 @@ package files
 
 import (
 	"database/sql"
+	"encoding/json"
+	"net/http"
+	"strconv"
 	"time"
+
+	"github.com/go-chi/chi"
 )
+
+func (h *handler) Modifier(rw http.ResponseWriter, rq *http.Request) {
+
+	id, err := strconv.Atoi(chi.URLParam(rq, "id"))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	file, err := Get(h.db, int64(id))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewDecoder(rq.Body).Decode(&file)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = file.Validate()
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = Modifier(h.db, int64(id), file)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(file)
+}
 
 func Modifier(db *sql.DB, id int64, file *File) error {
 	file.ModifiedAt = time.Now()
