aeedaa05c6a96a604aa7b7736b43c62f9d156460
Ricardo Viana <richard@Ricardos-MacBook-Pro.local>
2024-05-30 16:50:26 -0300

    Testando GET

--- internal/users/get_test.go
+++ internal/users/get_test.go
@@ -1,14 +1,70 @@
 package users
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
 	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-chi/chi"
 )
 
 func TestGet(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db}
+
+	u := User{
+		ID:   1,
+		Name: "Richard Viana",
+	}
+
+	u.SetPassword(u.Password)
+
+	var b bytes.Buffer
+	err = json.NewEncoder(&b).Encode(&u)
+	if err != nil {
+		t.Error(err)
+	}
+
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPut, "/{id}", &b)
+
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("id", "1")
+
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	rows := sqlmock.NewRows([]string{"id", "name", "login", "password", "created_at", "modified_at", "deleted", "last_login"}).
+		AddRow(1, "Richard", "richardluizv@gmail.com", "123456", time.Now(), time.Now(), false, time.Now())
+
+	mock.ExpectQuery(regexp.QuoteMeta(`select * from "users" where id=$1`)).
+		WillReturnRows(rows)
+
+	h.Get(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Error: %v", rr)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+func TestGetDatabse(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Error(err)
--- internal/users/list_test.go
+++ internal/users/list_test.go
@@ -1,6 +1,8 @@
 package users
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
 	"time"
@@ -9,6 +11,40 @@ import (
 )
 
 func TestList(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db}
+
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	rows := sqlmock.NewRows([]string{"id", "name", "login", "password", "created_at", "modified_at", "deleted", "last_login"}).
+		AddRow(1, "Richard", "richardluizv@gmail.com", "123456", time.Now(), time.Now(), false, time.Now()).
+		AddRow(2, "Bernardo", "bernardoluizv@gmail.com", "123456", time.Now(), time.Now(), false, time.Now())
+
+	mock.ExpectQuery(regexp.QuoteMeta(`select * from "users" where deleted = false`)).
+		WithArgs().
+		WillReturnRows(rows)
+
+	h.List(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Error: %v", rr)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+func TestListDatabase(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Error(err)
